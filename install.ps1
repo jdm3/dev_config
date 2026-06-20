f7aeea9e25ddaa3ec905c7ccd30b8e3945f8ad88
Montgomery, Jefferson <Jefferson.Montgomery+amdeng@amd.com>
2026-06-20 16:11:23 -0700

    Update

--- install.ps1
+++ install.ps1
@@ -1,33 +1,25 @@
-winget install --exact --id Kitware.CMake
-winget install --exact --id BurntSushi.ripgrep.MSVC
-winget install --exact --id sharkdp.fd
-winget install --exact --id LLVM.LLVM
-winget install --exact --id Neovim.Neovim
-winget install --exact --id OpenJS.NodeJS
-winget install --exact --id Winmerge.WinMerge
-
-$null = New-Item -ItemType Directory -Force -Path "$env:LocalAppData\nvim"
-$null = New-Item -ItemType Directory -Force -Path "$env:LocalAppData\nvim\colors"
-$null = New-Item -ItemType Directory -Force -Path "$env:LocalAppData\nvim\lsp"
+&winget install --exact --id Kitware.CMake
+&winget install --exact --id BurntSushi.ripgrep.MSVC
+&winget install --exact --id sharkdp.fd
+&winget install --exact --id LLVM.LLVM
+&winget install --exact --id Neovim.Neovim
+&winget install --exact --id OpenJS.NodeJS
+&winget install --exact --id WinMerge.WinMerge
 
 Write-Host "Add to your path:"
 Write-Host "    %ProgramFiles%\LLVM\bin"
-Write-Host "    %ProgramFiles%\CMake\bin"
-Write-Host "Press any key to continue..." -NoNewline
-$null = [Console]::ReadKey($true)
+Write-Host "    %HomeDrive%%HomePath%\AppData\Local\Programs\WinMerge"
+pause
 
 Write-Host "Run winmergeu and import settings (Edit -> options -> import) from winmerge/winmerge.ini"
-Write-Host "Press any key to continue..." -NoNewline
-$null = [Console]::ReadKey($true)
+pause
 
 Write-Host "Download a nerdfont version of your desired font: https://www.nerdfonts.com/font-downloads"
 Write-Host "Install it, and set it as your terminal font."
-Write-Host "Press any key to continue..." -NoNewline
-$null = [Console]::ReadKey($true)
+pause
 
 Write-Host "Diffing configuration..." -NoNewLine
-powershell -Command "Start-Process diff.ps1 -Verb RunAs"
-Write-Host "Press any key to continue..." -NoNewline
-$null = [Console]::ReadKey($true)
+Start-Process powershell.exe -Verb RunAs -ArgumentList "-NoProfile -ExecutoinPolicy Bypass -File $PSScriptDir\diff.ps1"
+pause
 
-nvim.exe "+Lazy! sync" +qa
+&$env:ProgramFiles\neovim\bin\nvim.exe "+Lazy! sync" +qa
